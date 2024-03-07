b605a388c34c76931955ea8e86114bc09b619240
PauloVerissimoCarvalho <paulohenrique7carvalho@gmail.com>
2024-03-07 17:28:55 -0300

    terminei

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
-
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
+function verificacao(){
+    let user= document.getElementById("usuario").value;
+    let password= document.getElementById("senha").value;
+    
+    if(user.trim() === "" || password.trim() === ""  ){
+        alert("Preencha todos os campos");
+    } else {
+        if(user.trim() === "adm" || password.trim() === "adm") {
+            window.location.href = ("./pages/inicial.html");
+        }
+        else{
+            alert("Usuario ou senha incorreto!");
+        }
+        
+    }
+}

6a66617661dca13e08e060ab4191212115ebe966
PauloHenriqueCarvalho <paulohenrique7carvalho@gmail.com>
2024-03-10 19:10:17 -0300

    dark-mode

--- js/script.js
+++ js/script.js
@@ -1,16 +1,44 @@
-function verificacao(){
-    let user= document.getElementById("usuario").value;
-    let password= document.getElementById("senha").value;
-    
-    if(user.trim() === "" || password.trim() === ""  ){
-        alert("Preencha todos os campos");
+function verificacao() {
+    let user = document.getElementById("usuario").value;
+    let password = document.getElementById("senha").value;
+
+    if (user.trim() === "" || password.trim() === "") {
+
     } else {
-        if(user.trim() === "adm" || password.trim() === "adm") {
-            window.location.href = ("./pages/inicial.html");
+        if (user.trim() === "adm" || password.trim() === "adm") {
+            Swal.fire(
+                {
+                    text: 'Bem Vindo!',
+                    icon: 'success',
+                    tittle: 'Atenção',
+
+                }
+            ).then((result) =>{
+                if(result.isConfirmed){
+                    window.open("./pages/inicial.html", "_self");
+                }
+
+            })  
         }
-        else{
-            alert("Usuario ou senha incorreto!");
+        else {
+            Swal.fire(
+                {
+                    text: 'Usuario ou Senha incorreta!',
+                    icon: 'error',
+                    tittle: 'Atenção',
+
+                }
+            )
         }
-        
+
     }
 }
+
+
+const $html = document.querySelector('html');
+const $checkbox = document.querySelector('#switch');
+
+$checkbox.addEventListener('change', function(){
+    $html.classList.toggle('dark-mode')
+})
+
